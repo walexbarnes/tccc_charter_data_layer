92b1a14288cc81d2ae6504ea302c1e98f12b30bc
walexbarnes <59946143+walexbarnes@users.noreply.github.com>
2024-03-11 14:22:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
 (function() {
     var dataLayer = window.dataLayer || [];
-    var userAction = prompt("Is the event tracking a user interaction or virtual page view? (user_interaction/virtual_page_view)");
+    function promptWithValidation(question, choices) {
+        var response;
+        do {
+            response = prompt(question + "\n" + choices.map((choice, index) => (index + 1) + ". " + choice).join("\n"));
+        } while (!choices.map((_, index) => String(index + 1)).includes(response));
+        return response;
+    }
+
+    var userAction = promptWithValidation(
+        "Is the event tracking a user interaction or virtual page view?",
+        ["user_interaction", "virtual_page_view"]
+    );
 
     var eventData = {
         event: "charter_event",
-        user_action: "",
+        user_action: userAction === "1" ? "interaction" : "virtual_page_view",
         user_action_detail: "",
         ux_element: "",
         child_element_text: "",
         child_ux_element_link: ""
     };
 
-    if (userAction === "virtual_page_view") {
-        eventData.user_action = "virtual_page_view";
+    if (userAction === "2") {
         var pageName = prompt("What is the name of the 'page' or screen you want to track?");
         eventData.user_action_detail = pageName;
-    } else if (userAction === "user_interaction") {
-        eventData.user_action = "interaction";
-        var interactionType = prompt("What best describes the type of user interaction you are hoping to track? (click, submit, play, pause, download, other)");
-        eventData.user_action_detail = interactionType;
+    } else if (userAction === "1") {
+        var interactionChoices = ["click", "submit", "play", "pause", "download", "other"];
+        var interactionType = promptWithValidation(
+            "What best describes the type of user interaction you are hoping to track?",
+            interactionChoices
+        );
+        eventData.user_action_detail = interactionChoices[parseInt(interactionType) - 1];
 
-        var elementType = prompt("What best describes the element type of interaction you are hoping to track? (button, header, footer, video, survey, form, image, carousel, hyperlink, accordion, tab, modal, input field, radio buttons, checkbox, social share, search, other)");
-        eventData.ux_element = elementType;
+        var elementTypeChoices = [
+            "button", "header", "footer", "video", "survey", "form", "image",
+            "carousel", "hyperlink", "accordion", "tab", "modal", "input field",
+            "radio buttons", "checkbox", "social share", "search", "other"
+        ];
+        var elementType = promptWithValidation(
+            "What best describes the element type of interaction you are hoping to track?",
+            elementTypeChoices
+        );
+        eventData.ux_element = elementTypeChoices[parseInt(elementType) - 1];
 
         var elementText = prompt("What best describes the text of the clicked element?");
         eventData.child_element_text = elementText;
 
-        var isLink = prompt("Is the element interaction a link that takes the user somewhere? (Y/N)");
+        var isLink = promptWithValidation(
+            "Is the element interaction a link that takes the user somewhere? (Y/N)",
+            ["Y", "N"]
+        );
         if (isLink.toUpperCase() === "Y") {
             var linkURL = prompt("What is the URL where the link takes the user?");
             eventData.child_ux_element_link = linkURL;
         } else {
-            // Removing link-related property if it's not a link
             delete eventData.child_ux_element_link;
         }
     } else {
